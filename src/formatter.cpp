2c8b9f2cfc287752add4a51b62a6da460c82ce59
Szymon P <szymon_jozef@proton.me>
2026-01-11 20:50:49 +0100

    rename replace to replace_once

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -3,8 +3,8 @@
 #include <sstream>
 #include <string>
 
-static bool replace(std::string &str, const std::string &from,
-                    const std::string &to) {
+static bool replace_once(std::string &str, const std::string &from,
+                         const std::string &to) {
   size_t start_pos = str.find(from);
   if (start_pos == std::string::npos)
     return false;
@@ -23,7 +23,7 @@ std::string inject_html_into_template(const std::string &FILE_NAME,
   std::ostringstream ss;
   ss << file.rdbuf();
   result = ss.str();
-  if (replace(result, TEMPLATE, HTML)) {
+  if (replace_once(result, TEMPLATE, HTML)) {
     return result;
   }
   return "";
